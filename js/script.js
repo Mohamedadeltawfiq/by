0c94f9489c3b14643f0bbc049af6186f723f8c9c
Mohamedadeltawfiq <mohamedtawfiktawfik123@gmail.com>
2024-08-27 16:16:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let nav_Menu = document.getElementById("menu");
+function open_close_menu() {
+    nav_Menu.classList.toggle("active")
+}
+
+function mytime()
+{
+    let time = new Date();
+    let hr = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    if( hr < 10)
+        {
+            hr = '0' + hr
+        }
+    if( hr >= 12)
+        {
+            hr = "PM " + hr
+        }
+    if( hr < 12)
+    {
+        hr = "AM " + hr
+    }
+    if( min < 10)
+        {
+            min = '0' + min
+        }
+    if( sec < 10)
+        {
+            sec = '0' + sec
+        }
+    document.getElementById('mytime').innerHTML = hr + ':' + min + ':' + sec;
+}
+window.onload = function()
+{
+    setInterval(mytime,500)
+}
+
+console.log("Welcome To ' Black & Yellow ' Website");
